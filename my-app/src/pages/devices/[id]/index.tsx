0c3e772d26015d601e72e58b3d6c61d462cd4165
KyryloPikhno <Mr.Good@i.ua>
2023-09-03 01:46:27 +0300

    config project

---
+++ my-app/src/pages/devices/[id]/index.tsx
@@ -0,0 +1,11 @@
+const Device = () => {
+
+
+  return (
+    <div>
+      Device
+    </div>
+  )
+}
+
+export default Device
---
+++ my-app/src/pages/devices/index.tsx
@@ -0,0 +1,13 @@
+import Forms from "../../../components/Forms"
+
+const Devices = () => {
+
+
+  return (
+    <div>
+     <Forms />
+    </div>
+  )
+}
+
+export default Devices
